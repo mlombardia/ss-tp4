40c8694a01f0a6de4b6793c588a963ef207b670f
Maximiliano Lombardia <mlombardia@itba.edu.ar>
2022-05-06 11:15:59 -0300

    feat: analytical implemented

--- src/main/java/ar/edu/itba/ss/tp4/App.java
+++ src/main/java/ar/edu/itba/ss/tp4/App.java
@@ -11,6 +11,7 @@ import ar.edu.itba.ss.tp4.simulation.SimulationController;
 public class App {
   public static void main( String[] args ) {
     Integrator beeman = new Beeman();
+    Integrator analytical = new Analytical(1, 100, Math.pow(10,4));
     SimulationController controller = new SimulationController(0.0001,beeman);
     controller.simulate();
   }
--- src/main/java/ar/edu/itba/ss/tp4/integrators/Analytical.java
+++ src/main/java/ar/edu/itba/ss/tp4/integrators/Analytical.java
@@ -5,8 +5,21 @@ import ar.edu.itba.ss.tp4.models.Particle;
 
 public class Analytical implements Integrator {
 
+  public double amplitude;
+  public double gamma;
+  public double k;
+  public double t;
+
+  public Analytical(double amplitude, double gamma, double k){
+    this.amplitude = amplitude;
+    this.gamma = gamma;
+    this.k = k;
+    this.t = 0;
+  }
+
   @Override
   public void updateData(double deltaT, Particle particle) {
-
+    this.time += deltaT;
+    particle.xPos = amplitude * Math.exp(-(this.gamma/(2*particle.weight))*this.time) * Math.cos(Math.sqrt((this.k/particle.weight)-(Math.pow(this.gamma,2)/(Math.pow(particle.weight,2)*4))* this.time))
   }
 }
