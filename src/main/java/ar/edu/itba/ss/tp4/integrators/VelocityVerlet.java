f71f2d9d5abf33d51022f8e221da3c88ef1056b8
Paula Domingues <pdomingues@itba.edu.ar>
2022-05-07 21:00:19 -0300

    fix: velocity verlet middle step added

--- src/main/java/ar/edu/itba/ss/tp4/integrators/VelocityVerlet.java
+++ src/main/java/ar/edu/itba/ss/tp4/integrators/VelocityVerlet.java
@@ -6,6 +6,10 @@ import static ar.edu.itba.ss.tp4.simulation.SimulationController.k;
 
 public class VelocityVerlet implements Integrator {
 
+    private double getForce(double position, double velocity) {
+        return -k * position - 100 * velocity;
+    }
+
     public void getNewPosition(double delta, Particle particle, double force) {
         particle.xPos = particle.xPos + delta * particle.xVel + (Math.pow(delta, 2) / particle.weight) * force;
     }
@@ -14,11 +18,19 @@ public class VelocityVerlet implements Integrator {
         particle.xVel = particle.xVel + (delta / (2 * particle.weight)) * (prevForce + newForce);
     }
 
+    private double getMiddleVel(double deltaT, Particle particle, double force) {
+        double delta = deltaT / 2;
+        double middlePosition = particle.xPos + delta * particle.xVel + (Math.pow(delta, 2) / particle.weight) * force;
+        double newForce = getForce(middlePosition, particle.xVel);
+        return particle.xVel + (delta / (2 * particle.weight)) * (force + newForce);
+    }
+
     @Override
     public void updateData(double deltaT, Particle particle) {
-        double force = -k * particle.xPos - 100 * particle.xVel;
+        double force = getForce(particle.xPos, particle.xVel);
+        double middleVel = getMiddleVel(deltaT, particle, force);
         getNewPosition(deltaT, particle, force);
-        double newForce = -k * particle.xPos - 100 * particle.xVel;
+        double newForce = getForce(particle.xPos, middleVel);
         getNewVelocity(deltaT, particle, force, newForce);
     }
 
